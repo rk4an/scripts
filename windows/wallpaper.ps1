9c60ad166fc3562b8efc4a59b734777c65dc8fb2
Erkan <rk4an@users.noreply.github.com>
2016-09-21 15:35:18 +0200

    Create wallpaper.ps1

---
+++ windows/wallpaper.ps1
@@ -0,0 +1,34 @@
+$user = Get-WmiObject win32_useraccount -Filter "name = 'Utilisateur'"
+$sid = $user.sid
+Set-Itemproperty -path "Registry::HKEY_USERS\${sid}\Control Panel\Desktop" -name WallPaper -value "C:\Airwatch\wallpaper.jpg"
+
+Move-Item C:\Airwatch\Products\Wallpaper\wallpaper.jpg C:\Airwatch\ -Force
+
+
+Add-Type @"
+using System;
+ using System.Runtime.InteropServices;
+ using Microsoft.Win32;
+
+ namespace Wallpaper
+ {
+   public enum Style : int
+   {
+     Tile, Center, Stretch, NoChange
+   }
+
+   public class Setter {
+     public const int SetDesktopWallpaper = 20;
+     public const int UpdateIniFile = 0x01;
+     public const int SendWinIniChange = 0x02;
+     [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+     private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+
+     public static void SetWallpaper ( string path, Wallpaper.Style style ) {
+       SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+     }
+   }
+ }
+"@
+
+[Wallpaper.Setter]::SetWallpaper('C:\Airwatch\wallpaper.jpg', 0)
