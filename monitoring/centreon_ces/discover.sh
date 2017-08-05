a733846b9d73a52d43e93e5f01e96db1805e62d8
Erkan <rk4an@users.noreply.github.com>
2017-08-05 20:59:21 +0200

    Create discover.sh

---
+++ monitoring/centreon_ces/discover.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SUBNET=$1
+NAME=$2
+PASSWORD=$3
+
+nmap -sP $SUBNET -oG - | awk -v NAME=$NAME -v PASSWORD=$PASSWORD '/Up$/{
+sub(/\(/,"",$3)
+sub(/\)/,"",$3)
+if($3=="") $3="HOST-"NR
+print "centreon -u "NAME" -p "PASSWORD" -o HOST -a ADD -v \""toupper($3)";"toupper($3)";"$2";generic-host;central\""}'
