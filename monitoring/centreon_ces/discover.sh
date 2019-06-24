40c44e7576cb6b755f99a6a7c58bf4653f8668b5
Erkan <rk4an@users.noreply.github.com>
2019-06-24 16:23:18 +0200

    Update discover.sh

--- monitoring/centreon_ces/discover.sh
+++ monitoring/centreon_ces/discover.sh
@@ -8,4 +8,4 @@ nmap -sP $SUBNET -oG - | awk -v NAME=$NAME -v PASSWORD=$PASSWORD '/Up$/{
 sub(/\(/,"",$3)
 sub(/\)/,"",$3)
 if($3=="") $3="HOST-"NR
-print "centreon -u "NAME" -p "PASSWORD" -o HOST -a ADD -v \""toupper($3)";"toupper($3)";"$2";generic-host;central\""}'
+print "centreon -u "NAME" -p "PASSWORD" -o HOST -a ADD -v \""toupper($3)";"toupper($3)";"$2";generic-active-host-custom;central;hostgroup\""}'
